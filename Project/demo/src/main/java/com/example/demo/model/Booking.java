c25018416cc859a892d85fbb075e0964fd3ea0f7
VoNgocThuy2005 <vothuyyy09@gmail.com>
2025-03-24 14:34:07 +0700

    Templates

--- Project/demo/src/main/java/com/example/demo/model/Booking.java
+++ Project/demo/src/main/java/com/example/demo/model/Booking.java
@@ -15,11 +15,59 @@ public class Booking {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
     @ManyToOne
     private Court court;
-    
+
     private LocalDateTime startTime;
     private LocalDateTime endTime;
     private String userEmail;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Court getCourt() {
+        return court;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setCourt(Court court) {
+        this.court = court;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public Booking(Long id, Court court, LocalDateTime startTime, LocalDateTime endTime) {
+        this.id = id;
+        this.court = court;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+    public Booking() {}
 }
--- Project/demo/src/main/java/com/example/demo/model/Court.java
+++ Project/demo/src/main/java/com/example/demo/model/Court.java
@@ -22,4 +22,52 @@ public class Court {
     
     @OneToMany(mappedBy = "court")
     private List<Booking> bookings;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
+
+    public Court(Long id, String name, String location, boolean available) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.available = available;
+    }
+    public Court() {}
 }
