ad2ef7bb6ee673a6d197836914824891ac4a4fb0
dinhtuandev <tuantd1384@ut.edu.vn>
2025-03-27 19:52:38 +0700

    update local host URL
    add scheduling framework, chỉnh lại đường dẫn localhost

--- Project/demo/src/main/java/com/example/demo/DemoApplication.java
+++ Project/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,25 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		openHomePage();
+	}
+
+	private static void openHomePage() {
+		String url = "http://localhost:8080";
+		try {
+			Runtime rt = Runtime.getRuntime();
+			rt.exec("rundll32 url.dll,FileProtocolHandler " + url);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
