a60e00dcc6aa43f44192017dc5085b81a2108db1
dinhtuandev <tuantd1384@ut.edu.vn>
2025-03-31 09:49:33 +0700

    fix local host
    fix localhost không hoạt động đúng

--- Project/demo/src/main/java/com/example/demo/DemoApplication.java
+++ Project/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,21 +3,23 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import java.awt.Desktop;
+import java.net.URI;
 
 @SpringBootApplication
 @EnableScheduling
-public class DemoApplication {
+public class DemoApplication implements ApplicationListener<ContextRefreshedEvent> {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		openHomePage();
 	}
 
-	private static void openHomePage() {
-		String url = "http://localhost:8080";
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent event) {
 		try {
-			Runtime rt = Runtime.getRuntime();
-			rt.exec("rundll32 url.dll,FileProtocolHandler " + url);
+			Desktop.getDesktop().browse(new URI("http://localhost:8080"));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
